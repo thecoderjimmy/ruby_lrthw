6f419ceef7887a0aac1cffeb7d6c56613a1c9edb
Jimmy <thecoderjimmy@gmail.com>
2019-03-12 09:09:55 +0900

    ruby review notes

---
+++ ex22.rb
@@ -0,0 +1,38 @@
+# Exercise 22: What Do You Know So Far?
+# https://learnrubythehardway.org/book/ex22.html
+
+# RUNNING A RUBY FILE
+#   in the command terminal: ruby rb1.rb
+
+# irb
+#   fires up ruby bash
+
+# exit
+#   leaves ruby bash
+
+
+
+# #
+#   used for comments or disabling code
+
+# puts
+#   prints out strings into terminal
+
+# "" or ''
+#   wraps around strings
+#   can be used inside each other to have quotes in strings
+
+# .to_f
+#   takes a string and converts into a number
+#   if the string has letters at the end they go away
+#     "123.45e1".to_f        #=> 1234.5
+#     "45.67 degrees".to_f   #=> 45.67
+#     "thx1138".to_f         #=> 0.0
+
+# %
+#    remainder
+
+# #{}
+#   insert this in a string to make computations in them
+#    puts "What is 3 + 2? #{3 + 2}"
+#    puts "What is 5 - 7? #{5 - 7}"
